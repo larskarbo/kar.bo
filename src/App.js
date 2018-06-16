1e428a19c960bb3583c2666c1cc5e7d4cc2252c5
Lars Karbo <mail@larskarbo.no>
2018-06-16 14:44:47 +0200

    pixi

--- src/App.js
+++ src/App.js
@@ -19,39 +19,46 @@ class App extends Component {
     };
 
     return (
+    <div>
+      <canvas className={"canvas " } width={0} height={0} ref={'asdf'}>
+           A chart
+         </canvas>
       <div style={{
-        height: '100vh'
-      }} ref="resizer">
-        <div style={{
-          width: '90vw',
-          height: '100vh'
-        }}>
-        <canvas className={"canvas " } width={1500} height={200} ref={'asdf'}>
-          A chart
-        </canvas>
-          <div style={{
-            position: 'absolute',
-            top: 1,
-            width: 1500,
-            height: 1500
-          }} id="pixi" width={1500} height={1500}>
-            <Stage options={OPTIONS} width={width} height={height}>
-              <Renderer />
-            </Stage>
-          </div>
-        </div>  
-        <button style={{
-          position: 'fixed',
-          bottom: 0,
-          right: 0,
-          width: 100,
-          height: 100,
-          backgroundColor: "rgba(0,0,0,0.02)",
-          border: 'none',
-          outline: 'none'
-        }} onTouchEnd={this.next} onMouseUp={this.next}>next</button>
-      </div>
-    );
+        // position: 'absolute',
+        // top: 0,
+        width: '100vw',
+          height: '100vh',
+          border: 'solid 1px black'
+        }}
+        ref={parent => this.parent = parent}
+          id="pixi"></div>
+        </div>
+    )
+    // return (
+    //   // <div style={{
+    //   //   height: '100vh'
+    //   // }} ref="resizer">
+    //   //   <div style={{
+    //   //     width: '90vw',
+    //   //     height: '100vh'
+    //   //   }}>
+    //     
+          
+            
+    //       {/* /* </div>
+    //     </div>  
+    //     <button style={{
+    //       position: 'fixed',
+    //       bottom: 0,
+    //       right: 0,
+    //       width: 100,
+    //       height: 100,
+    //       backgroundColor: "rgba(0,0,0,0.02)",
+    //       border: 'none',
+    //       outline: 'none'
+    //     }} onTouchEnd={this.next} onMouseUp={this.next}>next</button>
+    //   </div> */ */}
+    // );
   }
 
   next = () => {
@@ -67,7 +74,7 @@ class App extends Component {
     // returns {} if no data was loaded (you are on base napchart.com/)
     // returns the data if you are on napchart.com/xxxxx
     // console.log('☕️', this.props.initialData.chartData)
-    var napchart = new Renderer({ ctx })
+    var napchart = new Renderer({ container: this.parent, ctx })
     this.napchart = napchart
     // napchart.onUpdate = () => {
     //   // this.props.onUpdate()
@@ -76,11 +83,11 @@ class App extends Component {
     // for debugging
     window.napchart = napchart
 
-    canvas.oncontextmenu = function (event) {
-      event.preventDefault()
-      event.stopPropagation()
-      return false
-    }
+    // canvas.oncontextmenu = function (event) {
+    //   event.preventDefault()
+    //   event.stopPropagation()
+    //   return false
+    // }
 
     // this.props.setGlobalNapchart(napchart)
   }
